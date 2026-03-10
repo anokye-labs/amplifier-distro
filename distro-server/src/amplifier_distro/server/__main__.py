d8cfea219e02c683193bd979a50c133d208588bc
Paul Payne <paul@payne.io>
2026-03-10 16:35:28 -0700

    Delete server.

--- distro-server/src/amplifier_distro/server/__main__.py
+++
@@ -1,5 +0,0 @@
-"""Allow running the server with: python -m amplifier_distro.server"""
-
-from amplifier_distro.server.cli import serve
-
-serve()
--- distro-server/src/amplifier_distro/server/apps/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Distro Server Apps
-
-Each subdirectory is an app that can be auto-discovered and mounted.
-See app.py:DistroServer.discover_apps() for the discovery protocol.
-"""
